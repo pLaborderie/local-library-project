11a7f558f30fd4b6fadfa16489f8cb420e62e4d4
Paul Laborderie <paul.laborderie@gmail.com>
2021-01-09 03:20:29 +0100

    postgres connection for prod

--- knexfile.js
+++ knexfile.js
@@ -19,6 +19,18 @@ module.exports = {
     useNullAsDefault: true,
   },
 
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+    useNullAsDefault: true,
+  },
+
   // staging: {
   //   client: 'postgresql',
   //   connection: {
@@ -34,21 +46,5 @@ module.exports = {
   //     tableName: 'knex_migrations'
   //   }
   // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 
 };
